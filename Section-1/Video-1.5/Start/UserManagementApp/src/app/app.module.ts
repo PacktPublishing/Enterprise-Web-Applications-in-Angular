b7c6b28fb8faa80c013f04486b40032a0e09002a
Nitin Rastogi <nitin.rastogi@live.in>
2019-06-08 08:23:07 -0400

    Removed un used code
    Removed un used code

--- Section-1/Video-1.5/Start/UserManagementApp/src/app/app.module.ts
+++ Section-1/Video-1.5/Start/UserManagementApp/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { RouterModule, Routes } from "@angular/router";
 import { UserModule } from './user/user.module';
-
 
 
 
--- Section-1/Video-1.5/Start/UserManagementApp/src/app/user/user-detail/user-detail.component.ts
+++ Section-1/Video-1.5/Start/UserManagementApp/src/app/user/user-detail/user-detail.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { User } from '../IUser';
-import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { allUsers } from '../allUsers';
+
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
@@ -11,14 +8,9 @@ import { allUsers } from '../allUsers';
 })
 export class UserDetailComponent implements OnInit {
   selectedUser:User;
-  constructor(private route: ActivatedRoute) { }
-  idVal: string;
+  constructor() { }   
 
-  ngOnInit() {
-    this.route.paramMap.pipe(
-      switchMap((params: ParamMap) =>         
-         of(allUsers.find(p => p.EmpId == +params.get('id')))
-    )).subscribe(userDetail => this.selectedUser = userDetail);
+  ngOnInit() {   
    
   }
 
